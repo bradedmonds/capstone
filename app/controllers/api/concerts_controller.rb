a355c118217fe4f2283412f39ecc965582635949
Brad Edmonds <bcedmonds0417@gmail.com>
2021-04-25 11:32:56 -0500

    update concerts controller to send all data to frontend

--- app/controllers/api/concerts_controller.rb
+++ app/controllers/api/concerts_controller.rb
@@ -1,23 +1,7 @@
 class Api::ConcertsController < ApplicationController
   def index
-    city = City.find_by(city: params[:id].capitalize)
-    
-    # @concerts = city.concerts
-    
-    #get all the concerts by venue category - DONE
-    #order them by artist score
-    #limit to whatever number limit
-
-    venue_ids = []
-    venues = city.venues.where(category: "Large")
-    venues.each do |venue|
-      venue_ids << venue.id
-    end
-    
-    # ordered_concerts = []
-    @concerts = Concert.where(venue_id: [venue_ids])
-    # ordered_concerts << concerts[0]
-    # concerts = ordered_concerts
+    concerts = City.find_by(city: params[:id].capitalize).concerts
+    @concerts = concerts.order(:date)
     
     render "index.json.jb"
   end
